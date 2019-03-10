47d56f586d5f18ae13bba5c41bc4a17741903777
Ummer Khan <khanummergit@gmail.com>
2019-03-09 16:24:23 -0800

    adds delete mashup

--- src/MashupShow/MashupShow.js
+++ src/MashupShow/MashupShow.js
@@ -10,7 +10,7 @@ class MashupShow extends Component {
     componentDidMount(){
         this.getMashup(this.props.match.params.id)
     }
-    
+
     getMashup = async (id) => {
         try {
             const response = await fetch(`http://localhost:4000/mashups/${id}`, {
@@ -35,7 +35,29 @@ class MashupShow extends Component {
             return err
         }
     }
-    // do a turnary for if user is logged in and if logged user.id(pass from app.js) = this user.id then show delete button if not dont show delete button
+
+    deleteMashup = async (id) => {
+        try {
+            const deletedMashup = await fetch(`http://localhost:4000/mashups/${id}`, {
+            method: 'DELETE',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'http://localhost:3000'
+            }
+            });
+            if (!deletedMashup.ok) {
+            throw Error(deletedMashup.statusText);
+            }
+            const parsedDeletedMashup = await deletedMashup.json();
+            console.log(parsedDeletedMashup, 'deleted Mashup')
+        //   this.props.history.push('/');
+        } catch (err) {
+          return err
+        }
+      }
+
+    // do a turnary for if mashup belongs to loggedUser, pass in from App.js and store in state, if userId of mashup and loggedUser._id match then show delete button if not dont show delete button
   render() {
     return (
       <div className="MashupShow">
@@ -46,6 +68,7 @@ class MashupShow extends Component {
             <div>{this.state.mashup.Video}</div>
             <div>{this.state.mashup.GIF}</div>
             <div>{this.state.mashup.Votes}</div>
+            <button onClick={() => this.deleteMashup(this.state.mashup._id)}>Delete</button>
       </div>
     );
   }
